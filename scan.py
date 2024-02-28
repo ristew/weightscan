ede097e24f65e34421dd48798c2ed34d0981c960
Riley Stewart <me@rileystew.art>
2024-02-27 19:27:02 -0800

    tsne

--- scan.py
+++ scan.py
@@ -7,9 +7,10 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from umap import UMAP
+from openTSNE import TSNE
 import matplotlib.pyplot as plt
 from PIL import Image
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, BitsAndBytesConfig
 import seaborn as sns
 
 class Scan():
@@ -17,11 +18,24 @@ class Scan():
         torch.set_default_device('cuda')
         torch.set_float32_matmul_precision('medium')
         # model_name = 'MistralAI/Mistral-7B-v0.1'
-        model_name = 'microsoft/phi-2'
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype='auto', trust_remote_code=True)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+        # self.model_name = 'google/gemma-2b'
+        self.model_name = 'microsoft/phi-2'
+        quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_name, quantization_config=quantization_config, trust_remote_code=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, trust_remote_code=True)
         self.top_k = 50
-        self.prompt = '''Living in a simulation sucks because'''
+        self.prompt = '''After the fire comes'''
+
+    def gemma(self):
+        return self.model_name == 'google/gemma-2b'
+    def norm(self, state):
+        if self.gemma():
+            return self.model.model.norm(state)
+        elif self.model_name == 'microsoft/phi-2':
+            return self.model.model.final_layernorm(state)
+        else:
+            print('guessing norm', self.model_name)
+            return self.model.norm(state)
 
     def forward(self):
         enc = self.tokenizer(self.prompt, return_tensors='pt', return_attention_mask=False)
@@ -30,21 +44,21 @@ class Scan():
 
     def get_normed_states(self, output):
         hidden_states = output.hidden_states
-        normed_states = [self.model.model.final_layernorm(hs.squeeze(0)) for hs in hidden_states[:-1]]
+        print(hidden_states[0][0].shape)
+        normed_states = [self.norm(hs.squeeze(0)) for hs in hidden_states[:-1]]
         # output layer is already normed
-        normed_states.append(hidden_states[-1].squeeze(0))
+        final = hidden_states[-1]
+        normed_states.append(final.squeeze(0))
         self.normed_states = normed_states
         return normed_states
 
-    def umap(self):
-        umap_reducer = UMAP(n_components=2, metric='cosine', min_dist=0.0, n_neighbors=20)
-        embeddings = []
+    def embed(self):
         final = self.normed_states[-1]
-        basis = torch.stack([(0.5 * state + 0.5 * final) for state in self.normed_states[:-1]])
+        basis = torch.stack(self.normed_states)
         print('final normed state', final)
-        umap_reducer.fit(basis.cpu().detach().numpy().reshape(-1, basis.size(-1)))
-        print('umap fit, transforming...')
-        return [umap_reducer.transform(state.cpu().detach().numpy()) for state in self.normed_states]
+        tsne_reducer = TSNE(n_components=3, perplexity=20, metric='cosine').fit(basis.cpu().detach().numpy().reshape(-1, basis.size(-1)))
+        print('tsne fit, transforming...')
+        return [tsne_reducer.transform(state.cpu().detach().numpy()) for state in self.normed_states]
 
     def logits(self, layer=-1):
         return self.model.lm_head(self.normed_states[layer]).float()
@@ -56,6 +70,8 @@ class Scan():
     def plot_embedding(self, embedding, layer_id):
         plt.figure(figsize=(10, 10))
         ax = sns.kdeplot(x=embedding[:, 0], y=embedding[:, 1], cmap="mako", levels=50)
+        plt.scatter(x=embedding[:, 0], y=embedding[:, 1], c="white", edgecolors="white")
+
         ax.set_facecolor('black')
         plt.gcf().set_facecolor('black')
         ax.xaxis.label.set_color('white')
@@ -84,7 +100,8 @@ class Scan():
     def test(self):
         output = self.forward()
         self.get_normed_states(output)
-        embeddings = self.umap()
+        embeddings = self.embed()
+        print('embeddings', embeddings)
         self.global_x_min = min(embedding[:, 0].min() for embedding in embeddings)
         self.global_x_max = max(embedding[:, 0].max() for embedding in embeddings)
         self.global_y_min = min(embedding[:, 1].min() for embedding in embeddings)
